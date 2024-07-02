59de5f7a265a1b14bc74e546a45d261f4be773ca
HP <HP@support.wondershare.net>
2024-07-02 14:15:27 +0530

    pom class update

--- GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/LeftPanel.java
+++ GUISeleniumFramework/src/main/java/com/comcast/crm/objectrepositoryutility/LeftPanel.java
@@ -14,11 +14,23 @@ public class LeftPanel {
 	@FindBy(xpath = "//span[text()=' Patients ']")
 	private WebElement patient;
 	
+	@FindBy(xpath = "//span[text()=' Manage Patients ']")
+	private WebElement manageptnt;
+	
 	@FindBy(xpath = "//span[text()=' Appointment History ']")
 	private WebElement appointmntHstry;
 	
 	@FindBy(xpath = "//span[text()=' Conatctus Queries ']")
-	private WebElement queries;
+	private WebElement ContctedQueries;
+	
+	@FindBy(xpath = "//span[text()=' Unread Query ']")
+	private WebElement unrdQuers;
+	
+	@FindBy(xpath = "//span[text()=' Read Query ']")
+	private WebElement ReadQuers;
+	
+	@FindBy(xpath = "//span[text()='B/w dates reports ']")
+	private WebElement BtwReprts;
 	
 	@FindBy(xpath = "//span[text()=' Reports ']")
 	private WebElement reports;
