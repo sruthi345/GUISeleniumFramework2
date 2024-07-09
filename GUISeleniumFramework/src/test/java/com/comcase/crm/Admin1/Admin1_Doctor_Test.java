bd42f549b0bad75e584c1555ebea8515ebb34557
Dell Inspiron15 <Dell Inspiron15@DESKTOP-VBUECEK>
2024-07-09 15:51:58 +0530

    check

--- GUISeleniumFramework/src/test/java/com/comcase/crm/Admin1/Admin1_Doctor_Test.java
+++ GUISeleniumFramework/src/test/java/com/comcase/crm/Admin1/Admin1_Doctor_Test.java
@@ -29,7 +29,7 @@ import com.comcast.crm.objectrepositoryutility.PatientRegisterPage;
  * @author Nithish
  * this class consists of add doctor script, editing doctor detalis script 
  * and deleting doctor script
- *hi
+ *hello
  *
  */
 @Listeners(com.comcast.crm.generic.listenerutility.ListImpClass.class)
